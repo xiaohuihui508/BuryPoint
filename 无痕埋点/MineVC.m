fb9b1b67c0168480995ff2b6fac5180907d0b389
xiaohuihui508 <15136718290@163.com>
2019-01-03 10:28:03 +0800

    测试新增

---
+++ 无痕埋点/MineVC.m
@@ -0,0 +1,34 @@
+//
+//  MineVC.m
+//  无痕埋点
+//
+//  Created by isoft on 2019/1/3.
+//  Copyright © 2019 isoft. All rights reserved.
+//
+
+#import "MineVC.h"
+
+@interface MineVC ()
+
+@end
+
+@implementation MineVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor purpleColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
